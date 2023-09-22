4e0a688b0d3e2ed8491e50e2962471b9c8c993db
Shihab Tawsif Shown <sadadrana@gmail.com>
2023-09-22 14:38:42 +0600

    Just Commit

---
+++ untitled/src/LengthofLastWord.java
@@ -0,0 +1,7 @@
+public class LengthofLastWord {
+    public int lengthOfLastWord(String s){
+        String[] arr = s.split(" ");
+        int len = arr.length;
+        return arr[len-1].length();
+    }
+}
