b22fb7f4a8e74bc16201b541ada83e523cdd1a33
Shihab Tawsif Shown <sadadrana@gmail.com>
2023-09-29 19:29:06 +0600

    Just Commit

--- untitled/src/RansomNote.java
+++ untitled/src/RansomNote.java
@@ -1,17 +1,23 @@
-import java.util.HashMap;
+import java.util.ArrayList;
 
 public class RansomNote {
     public boolean canConstruct(String ransomNote, String magazine) {
         int len =  ransomNote.length(), len2= magazine.length();
-        HashMap<Integer,String> map = new HashMap<>();
-        int j=0;
-        for (int i=0; i<len2; i+=len){
-            String string;
-            string = magazine.substring(i,i+len);
-            map.put(j,string);
-            j++;
+        ArrayList<Character> list = new ArrayList<>();
+        for (int i=0; i<len2; i++){
+            list.add(magazine.charAt(i));
         }
-        System.out.println(map);
+        Character c;
+        for (int i = 0; i<len; i++){
+            c = ransomNote.charAt(i);
+
+            if(list.contains(c)){
+                list.remove(c);
+            }
+            else
+                return false;
+        }
+
 
         return true;
     }
