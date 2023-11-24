6925b1dd3c028909608d0595497184ea41896ce9
Shihab Tawsif Shown <sadadrana@gmail.com>
2023-11-25 05:01:56 +0600

    Just Commit

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,19 +1,37 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String str = in.nextLine();
-        //String str2 = in.nextLine();
 
-        ValidParentheses solution = new ValidParentheses();
-        boolean ans = solution.isValid(str);
+        Integer[] values = {1,3};
+        Integer[] values2= {1,1,2};
+
+        // Create the binary tree
+        TreeNode p = createBinaryTree(values,0);
+        TreeNode q = createBinaryTree(values2, 0);
+        SameTree solution = new SameTree();
+
+        boolean ans = solution.isSameTree(p,q);
         System.out.println(ans);
-//        for (int i = 0; i < k; i++) {
-//            System.out.print(nums1[i]);
-//            if (i != k - 1) {
-//                System.out.print(",");
-//            }
-//        }
+    }
+    private static TreeNode createBinaryTree(Integer[] values) {
+        if (values == null || values.length == 0) {
+            return null;
+        }
+
+        return createBinaryTree(values, 0);
+    }
+
+    private static TreeNode createBinaryTree(Integer[] values, int index) {
+        if (index < values.length && values[index] != null) {
+            TreeNode node = new TreeNode(values[index]);
+            node.left = createBinaryTree(values, 2 * index + 1);
+            node.right = createBinaryTree(values, 2 * index + 2);
+            return node;
+        }
+
+        return null;
     }
 }
